9c36df9affb379b11697a2f9611b75f991e0f0ee
Shubham Khanna <105316553+Shubhk0@users.noreply.github.com>
2025-03-02 00:16:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, 'undici'];
+    return config;
+  },
+};
